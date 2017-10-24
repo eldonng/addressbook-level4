48ff0f5fbba1b205789a47fa3a3acd55d175239c
Eldon\eldonng <eldonng95@gmail.com>
2017-10-25 00:50:54 +0800

    change personcard and browserwindow UI

--- src/main/java/seedu/address/ui/BrowserPanel.java
+++ src/main/java/seedu/address/ui/BrowserPanel.java
@@ -6,14 +6,19 @@ import java.util.logging.Logger;
 import com.google.common.eventbus.Subscribe;
 
 import javafx.application.Platform;
+import javafx.beans.binding.Bindings;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Region;
+import javafx.scene.text.Text;
 import javafx.scene.web.WebView;
 import seedu.address.MainApp;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
 import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.model.tag.Tag;
 
 /**
  * The Browser Panel of the App.
@@ -21,49 +26,68 @@ import seedu.address.model.person.ReadOnlyPerson;
 public class BrowserPanel extends UiPart<Region> {
 
     public static final String DEFAULT_PAGE = "default.html";
-    public static final String GOOGLE_SEARCH_URL_PREFIX = "https://www.google.com.sg/search?safe=off&q=";
-    public static final String GOOGLE_SEARCH_URL_SUFFIX = "&cad=h";
 
     private static final String FXML = "BrowserPanel.fxml";
 
     private final Logger logger = LogsCenter.getLogger(this.getClass());
 
     @FXML
-    private WebView browser;
+    private Label name;
+    @FXML
+    private Label phone;
+    @FXML
+    private Label email;
+    @FXML
+    private Label address;
+    @FXML
+    private Text initials;
+    @FXML
+    private FlowPane tags;
+
 
     public BrowserPanel() {
         super(FXML);
 
         // To prevent triggering events for typing inside the loaded Web page.
         getRoot().setOnKeyPressed(Event::consume);
-
-        loadDefaultPage();
         registerAsAnEventHandler(this);
     }
 
     private void loadPersonPage(ReadOnlyPerson person) {
-        loadPage(GOOGLE_SEARCH_URL_PREFIX + person.getName().fullName.replaceAll(" ", "+")
-                + GOOGLE_SEARCH_URL_SUFFIX);
+        loadPage(person);
     }
 
-    public void loadPage(String url) {
-        Platform.runLater(() -> browser.getEngine().load(url));
+
+    public void loadPage(ReadOnlyPerson person) {
+        initials.setText(person.getName().fullName.substring(0,1));
+        name.textProperty().bind(Bindings.convert(person.nameProperty()));
+        phone.textProperty().bind(Bindings.convert(person.phoneProperty()));
+        address.textProperty().bind(Bindings.convert(person.addressProperty()));
+        email.textProperty().bind(Bindings.convert(person.emailProperty()));
+        initTags(person);
     }
 
-    /**
-     * Loads a default HTML file with a background that matches the general theme.
-     */
-    private void loadDefaultPage() {
-        URL defaultPage = MainApp.class.getResource(FXML_FILE_FOLDER + DEFAULT_PAGE);
-        loadPage(defaultPage.toExternalForm());
+    private void initTags(ReadOnlyPerson person) {
+        tags.getChildren().clear();
+        person.getTags().forEach(tag -> {
+            Label tagLabel = new Label(tag.tagName);
+            setTagColour(tagLabel, tag, person);
+            tags.getChildren().add(tagLabel);
+        });
     }
 
-    /**
-     * Frees resources allocated to the browser.
-     */
+    private void setTagColour(Label tagLabel, Tag tag, ReadOnlyPerson person) {
+        if (person.getTagColours().containsKey(tag)) {
+            tagLabel.setStyle("-fx-background-color: " + person.getTagColours().get(tag));
+        } else {
+            tagLabel.setStyle("-fx-background-color: blue");
+        }
+    }
+
     public void freeResources() {
-        browser = null;
+
     }
+
 
     @Subscribe
     private void handlePersonPanelSelectionChangedEvent(PersonPanelSelectionChangedEvent event) {
